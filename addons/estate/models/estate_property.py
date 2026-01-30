ee7e37fe3e446d8978849e10543b49c87f75358b
Hung <cshwk2020@gmail.com>
2026-01-31 04:35:56 +0800

    commit estate module :: CH8~comput fields and onchanges...v1

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,20 +1,33 @@
-from odoo import models, fields
+from odoo import api, models, fields
 from datetime import timedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Test Model"
 
+    #
+    total_area = fields.Float( compute="_compute_total_area", store=True )
+
+    @api.depends("living_area", "garden_area")
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    #
     property_type_id = fields.Many2one(
         "estate.property.type",
         string="Property Type"
     )
 
+    partner_id = fields.Many2one( "res.partner",
+                              string="Partner",
+                              ondelete="set null",
+                              copy=False)
 
-    #
-    partner_id = fields.Many2one("res.partner", string="Buyer", copy=False)
-    user_id = fields.Many2one("res.users", string="Salesperson",
-                                     default=lambda self: self.env.user)
+
+    user_id = fields.Many2one("res.users",
+                              string="Salesperson",
+                              default=lambda self: self.env.user)
 
     tag_ids = fields.Many2many( "estate.property.tag", string="Tags" )
 
@@ -25,10 +38,29 @@ class EstateProperty(models.Model):
     )
 
 
-
     #
     name = fields.Char(string="Name")
-    description = fields.Text(string="Description")
+    # description = fields.Text(string="Description")
+    description = fields.Char(compute="_compute_description")
+    @api.depends("partner_id.name")
+    def _compute_description(self):
+        for record in self:
+            record.description = "Test for partner %s" % record.partner_id.name
+
+    best_price = fields.Float( string="Best Offer",
+                               compute="_compute_best_price",
+                               store=True )
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            prices = record.offer_ids.mapped("price")
+            record.best_price = max(prices) if prices else float('-inf')
+
+
+
+
+
     postcode = fields.Char(string="Postcode")
 
     date_availability = fields.Date(
