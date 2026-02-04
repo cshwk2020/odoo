40989b8c781f2ebec7961ead75f6a9d7aca1d8b3
Hung <cshwk2020@gmail.com>
2026-02-05 05:27:01 +0800

    Initial commit ch11 estate module ~ inherited view, view_users_form_inherit_estate.xml

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -9,7 +9,8 @@
         'views/estate_property_tag_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_views.xml',
-        'views/estate_property_offer_form.xml'
+        'views/estate_property_offer_form.xml',
+        'views/users_form_inherit_estate_views.xml',
     ],
     'installable': True,
     'application': True,
--- addons/estate/models/__init__.py
+++ addons/estate/models/__init__.py
@@ -2,5 +2,6 @@ from . import estate_property_tag
 from . import estate_property_type
 from . import estate_property
 from . import estate_property_offer
+from . import estate_property_salesperson
 
 
