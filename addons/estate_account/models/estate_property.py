ca063dd1533caa1f96270b7381b79c8f7ff43103
Hung <cshwk2020@gmail.com>
2026-02-05 23:02:41 +0800

    Initial commit ch13 estate module ~ estate--acccount,override action_sold to patch add 2 invoice line items, 6% commision and admin fee 00

--- addons/estate_account/models/estate_property.py
+++ addons/estate_account/models/estate_property.py
@@ -16,17 +16,37 @@ class EstateProperty(models.Model):
         # Call original logic
         res = super().action_sold()
 
-        # Create a simple invoice
+
+
+        # Create a simple invoice::line items as account_move_line
+        commission = self.selling_price * 0.06
+        admin_fee = 100.00
+
         move_vals = {
             'move_type': 'out_invoice',
             'partner_id': self.buyer_id.id,
             'invoice_date': fields.Date.today(),
-            'invoice_line_ids': [(0, 0, {
-                'name': self.name,
-                'quantity': 1,
-                'price_unit': self.selling_price,
-                'tax_ids': [(6, 0, [])],  # <-- no taxes
-            })],
+            'invoice_line_ids': [
+                (0, 0, {
+                    'name': self.name,
+                    'quantity': 1,
+                    'price_unit': self.selling_price,
+                    'tax_ids': [(6, 0, [])],  # <-- no taxes
+                }),
+                (0, 0, {
+                    'name': "Commission (6% of selling price)",
+                    'quantity': 1,
+                    'price_unit': commission,
+                    'tax_ids': [(6, 0, [])],  # no taxes
+                }),
+                (0, 0, {
+                    'name': "Administrative fees",
+                    'quantity': 1,
+                    'price_unit': admin_fee,
+                    'tax_ids': [(6, 0, [])],  # no taxes
+                }),
+
+            ],
         }
 
         invoice = self.env['account.move'].create(move_vals)
